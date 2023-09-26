26ef8d18cbbed390f6bf635d228750f1b3a2353e
dlangto <dlangto@clemson.edu>
2023-09-26 12:37:35 -0400

    followers

--- run_parse.py
+++ run_parse.py
@@ -1,6 +1,7 @@
 import json
 import pandas
 import os
+import glob
 
 if not os.path.exists("parsed_files"):
 	os.mkdir("parsed_files")
@@ -8,41 +9,40 @@ if not os.path.exists("parsed_files"):
 
 dataset = pandas.DataFrame()
 
-json_file_name = "json_files/dAAAb.json"
+for json_file_name in glob.glob("json_files/*.json"):
+	
 
-f = open(json_file_name, "r")
-json_data = json.load(f)
-f.close()
+	f = open(json_file_name, "r")
+	json_data = json.load(f)
+	f.close()
 
 
 
-gh_id = json_data['login']
-gh_number_id = json_data['id']
-updated_at = json_data['updated_at']
-followers = json_data['followers']
-name = json_data['name']
+	gh_id = json_data['login']
+	gh_number_id = json_data['id']
+	updated_at = json_data['updated_at']
+	followers = json_data['followers']
+	name = json_data['name']
+	followers_url = json_data['followers_url']
 
-print(gh_id)
-print(gh_number_id)
-print(updated_at)
-print(followers)
-print(name)
+	print(gh_id)
+	print(gh_number_id)
+	print(updated_at)
+	print(followers)
+	print(name)
+	print(followers_url)
 
-row = pandas.DataFrame.from_records(
-	[
-	{
-		'gh_id': gh_id,
-		'gh_number_id': gh_number_id,
-		'updated_at': updated_at,
-		'followers': followers,
-		'name': name
+	row = pandas.DataFrame.from_records([{
+			'gh_id': gh_id,
+			'gh_number_id': gh_number_id,
+			'updated_at': updated_at,
+			'followers': followers,
+			'name': name,
+			'followers_url': followers_url
+			}])
 
-	}
-	]
-	)
-
-print(row)
-dataset = pandas.concat([dataset, row])
+	print(row)
+	dataset = pandas.concat([dataset, row])
 
 dataset.to_csv("parsed_files/github_user_data.csv"
 				, index=False)
